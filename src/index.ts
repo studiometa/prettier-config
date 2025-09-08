c39eb772484949af30532d58cc994dee3953bafd
Titouan Mathis <titouan@studiometa.fr>
2025-09-08 18:48:22 +0200

    Add support for arrow functions in filters

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,8 @@ export default {
     {
       files: '**/*.twig',
       options: {
-        twigPrintWidth: 100,
         twigMultiTags: ['html_element,end_html_element', 'with,endwith', 'apply,endapply'],
-        plugins: [import.meta.resolve('@afshinhaghighat/prettier-plugin-twig-melody')],
+        plugins: [import.meta.resolve('@zackad/prettier-plugin-twig')],
       },
     },
     {
